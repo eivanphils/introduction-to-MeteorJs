c76c6519e09774faebb42191187ad78ed7c0ef50
eivanphils <eivanphils@gmail.com>
2016-01-26 09:31:47 -0430

    add files to project

---
+++ phils-meteor-coursera.js
@@ -0,0 +1,28 @@
+if (Meteor.isClient) {
+  var img_data = [
+  {
+    img_src: "meteor.png",
+    img_alt: "imagen de meteor"
+  },
+  {
+    img_src: "node.png",
+    img_alt: "imagen de node"
+  },
+  {
+    img_src: "mongodb.png",
+    img_alt: "imagen de mongodb"
+  }
+  ];
+
+  Template.images.helpers({images:img_data});
+
+  Template.images.events({
+    'mouseenter .js-image': function(event){
+      $(event.target).css("width", "50px");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  console.log("i am the server");
+}
